81417000151dff5127312fa645620509b33dffd4
AlexisGood100 <89260957+AlexisGood100@users.noreply.github.com>
2024-12-27 07:25:37 -0500

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// Sample route
+
 router.get('/message', (req, res) => {
-  res.json({ message: 'Hello from backend API!' });
+  res.json({ message: 'Hello World!' });
 });
 
 module.exports = router;
